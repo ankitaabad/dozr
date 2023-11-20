25e5314b0340eaec4b2789a1776bb5cf8b511524
ankit <ankitaabad.1@gmail.com>
2023-11-20 08:21:28 +0530

    add captain definition file for cron

--- stocksCron/main.go
+++ stocksCron/main.go
@@ -12,7 +12,7 @@ func main() {
 	var client = &http.Client{
 		Timeout: 10 * time.Second,
 	}
-fmt.Println(os.Getenv("healthchecks_uuid"))
+fmt.Println("env uuid",os.Getenv("healthchecks_uuid"))
 	_, err := client.Head("http://cron-monitor.68.183.85.136.nip.io/ping/2a2491a1-87f2-4014-af1a-1f3bd63ca81c")
 	if err != nil {
 		fmt.Printf("%s", err)
