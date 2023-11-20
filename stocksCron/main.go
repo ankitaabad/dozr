939d9b5e8b25f822445c427ab6bc29931d5a6b45
ankit <ankitaabad.1@gmail.com>
2023-11-20 07:27:58 +0530

    add timezone and test caprover env variable

--- stocksCron/main.go
+++ stocksCron/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -11,7 +12,7 @@ func main() {
 	var client = &http.Client{
 		Timeout: 10 * time.Second,
 	}
-
+fmt.Println(os.Getenv("healthchecks_uuid"))
 	_, err := client.Head("http://cron-monitor.68.183.85.136.nip.io/ping/2a2491a1-87f2-4014-af1a-1f3bd63ca81c")
 	if err != nil {
 		fmt.Printf("%s", err)
