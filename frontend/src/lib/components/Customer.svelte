8bcb67cad7e42441a97ee0306ce9f8e7875ce379
ankit <ankitaabad.1@gmail.com>
2023-11-28 08:51:15 +0530

    files

--- frontend/src/lib/components/Customer.svelte
+++ frontend/src/lib/components/Customer.svelte
@@ -1,3 +1,52 @@
+<script>
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+
+	// if (browser) {
+	//     const socket = new WebSocket("ws://gotify.68.183.85.136.nip.io/stream?token=Cr7tCz6GvMo3jKG")
+	//     socket.addEventListener("open", ()=> {
+	//         console.log("Opened")
+	//     })
+	// }
+	let ws;
+
+	// const es = new EventSource("http://mercure.68.183.85.136.nip.io/.well-known/mercure?topic=http://example.com/book",{withCredentials:false})
+	// es.onmessage = function (e){
+	//   console.log("received data")
+	//   console.log(JSON.parse(e.data))
+	// }
+
+	onMount(() => {
+		try {
+			ws = new WebSocket('ws://gotify.68.183.85.136.nip.io/stream?token=Cr7tCz6GvMo3jKG');
+
+			console.log('waiting');
+		} catch (err) {
+			console.log(JSON.stringify(err));
+		}
+
+		ws.onopen = function () {
+			//  Web Socket is connected, send data using send()
+			//  ws.send("Message to send");
+
+			console.log('socket openend');
+		};
+
+		ws.onmessage = function (evt) {
+			console.log(evt);
+			var received_msg = evt.data;
+			console.log({ received_msg });
+			return false;
+		};
+
+		ws.onclose = function (event) {
+			// websocket is closed.
+      console.log({data: event})
+			alert('Connection is closed...');
+		};
+	});
+</script>
+
 <!-- <script>
 	import { createQuery } from '@tanstack/svelte-query';
 	import RecentTransaction from './RecentTransaction.svelte';
@@ -29,4 +78,4 @@
 	</div>
 {/if} -->
 
-<div> Cutomer</div>
+<div>Cutomer</div>
--- stocksCron/main.go
+++ stocksCron/main.go
@@ -5,17 +5,36 @@ import (
 	"net/http"
 	"os"
 	"time"
+	"github.com/jackc/pgx/v5"
+
 )
 
 func main() {
 	fmt.Println("Running the cron ")
-	var client = &http.Client{
-		Timeout: 10 * time.Second,
-	}
-	healthChecksBaseURL := os.Getenv("healthChecksBaseURL")
-	fmt.Println("hc base url", healthChecksBaseURL)
-	_, err := client.Head(healthChecksBaseURL)
+	// var client = &http.Client{
+	// 	Timeout: 10 * time.Second,
+	// }
+	// healthChecksBaseURL := os.Getenv("healthChecksBaseURL")
+	// fmt.Println("hc base url", healthChecksBaseURL)
+	// _, err := client.Head(healthChecksBaseURL)
+	// if err != nil {
+	// 	fmt.Printf("%s", err)
+	// }
+	fmt.Println(os.Getenv("database_url"))
+	conn, err := pgx.Connect(context.Background(), os.Getenv("database_url"))
 	if err != nil {
-		fmt.Printf("%s", err)
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
 	}
+	defer conn.Close(context.Background())
+
+	var name string
+	var weight int64
+	err = conn.QueryRow(context.Background(), "select name, weight from widgets where id=$1", 42).Scan(&name, &weight)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(name, weight)
 }
