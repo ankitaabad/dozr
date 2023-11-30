893b6892ced83cca192e709ca4ed1f6c366c4ddd
ankit <ankitaabad.1@gmail.com>
2023-11-30 12:08:30 +0530

    fix build

--- frontend/src/routes/test/+page.ts
+++ frontend/src/routes/test/+page.ts
@@ -1,5 +1,5 @@
 import { recentTransactionsStore, topStockGainersStore } from "$lib/store";
-
+export const prerender = true
 /** @type {import('./$types').PageLoad} */
 export async function load() {
 	await recentTransactionsStore.fetchData()
